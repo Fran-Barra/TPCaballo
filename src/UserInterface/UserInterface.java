18b7acb20a3bae654ba3c57e59977e4e33b96495
Your Name <you@example.com>
2022-04-14 22:43:38 -0300

    UserInterface

--- src/UserInterface/UserInterface.java
+++ src/UserInterface/UserInterface.java
@@ -3,9 +3,12 @@ package UserInterface;
 import java.util.Scanner;
 
 public class UserInterface {
-    static void askInitialPos(){
+    public static void askInitialPos(){
         Scanner inputScaner = new Scanner(System.in);
         System.out.println("Log the initial position of the horse (A1):");
         String pos = inputScaner.nextLine();
+        if (pos.length() != 2){
+            System.out.println("pos takes one character from A to H and a number from 1 to 8");
+        }
     }
 }
