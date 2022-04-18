5f327de51f6035dddd52ad98260d13fd6a4eea5f
Your Name <you@example.com>
2022-04-18 19:51:08 -0300

    stack

---
+++ src/Main/Tools/Stack.java
@@ -0,0 +1,42 @@
+package Tools;
+
+public class Stack<T> {
+    private T[] stack;
+    //is the position of the last element
+    private int top;
+    private int size;
+
+    public Stack() {
+        this.size = 10;
+        this.stack = (T[])new Object[this.size];
+        this.top = -1;
+    }
+
+    public void push(T o){
+        if (size() == this.size)
+            expandArray();
+        this.stack[++this.top] = o;
+    }
+
+    public void pop(){
+        //algoritmo de orden 1, siempre combiene ahorrar tiempo
+        //en casos de datos que ocupen mas memoria sera visto mas adelante
+        this.top--;
+    }
+
+    public T peek() {
+        if (isEmpty())
+            return null;
+        return this.stack[this.top];}
+
+    public boolean isEmpty() {return top == -1;}
+
+    public int size() {return this.top;}
+
+    private void expandArray(){
+        T[] auxArray = (T[])new Object[size + 10];
+        for (int i= 0; i < this.stack.length; i++){auxArray[i] = stack[i];}
+        this.size = auxArray.length;
+        this.stack = auxArray;
+    }
+}
