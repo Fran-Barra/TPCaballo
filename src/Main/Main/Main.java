c3f437a688ba27c967dffea8d9d92a4aeb41334b
luckylovaglio <luckylovaglio@gmail.com>
2022-04-19 17:20:52 -0300

    MovementAlgorithm update

--- src/Main/Main/Main.java
+++ src/Main/Main/Main.java
@@ -1,13 +1,31 @@
 package Main;
 
-import Tools.Stack;
-import UserInterface.UserInterface;
+import MovementAlgorithm.MovementAlgorithm;
+//import UserInterface.UserInterface;
+
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<byte[]>[] aux = new Stack[5];
-        Stack<byte[]> stuck1 = new Stack<byte[]>();
-        aux[0] = stuck1;
-        aux[0].push(new byte[] {1,2});
+//        Stack<byte[]>[] aux = new Stack[5];
+//        Stack<byte[]> stuck1 = new Stack<byte[]>();
+//        aux[0] = stuck1;
+//        aux[0].push(new byte[] {1,2});
+//        Stack<byte[]>[] stacks, ArrayList<byte[]> path;
+        int n = 2;
+        Stack<byte[]>[] stacks = new Stack[n];
+        ArrayList<ArrayList<byte[]>> paths = new ArrayList<>();
+        MovementAlgorithm.backTracking(new byte[] {2,2}, (byte) n, new byte[] {-1, -1}, stacks, paths);
+        String text = "";
+        for (int i = 0; i < paths.size(); i ++){
+            text += "<<" + (i + 1) + ">>\t(" ;
+            for (int j = 0; j < paths.get(i).size(); j ++){
+                text += "(" + paths.get(i).get(j)[0] + "," + paths.get(i).get(j)[1] + ")  ";
+            }
+            text += ")\n";
+            System.out.println(text);
+
+        }
     }
 }
--- src/Main/MovementAlgorithm/MovementAlgorithm.java
+++ src/Main/MovementAlgorithm/MovementAlgorithm.java
@@ -1,4 +1,60 @@
 package MovementAlgorithm;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class MovementAlgorithm {
+
+    public static void  backTracking(byte[] HorsePosition, byte n, byte[] previousPos, Stack<byte[]>[] stacks, ArrayList<ArrayList<byte[]>> paths){
+        if (n == 0){
+            ArrayList<byte[]> path = new ArrayList<>();
+            for (int i = stacks.length - 1; i >= 0; i--)
+                path.add(stacks[i].peek());
+            paths.add(path);
+            stacks[stacks.length - 1].pop();
+        }
+        else {
+            byte minX = (byte) (HorsePosition[0] - 2);
+            byte maxX = (byte) (HorsePosition[0] + 2);
+            byte minY = (byte) (HorsePosition[1] - 2);
+            byte maxY = (byte) (HorsePosition[1] + 2);
+            Stack<byte[]> validPositions = new Stack<>();
+            for (int i = 0; i <= 4; i++){
+                if ((i+1) % 2 == 0){
+                    validPositions.push(new byte[]{minX, (byte)(minY + i)});
+                    validPositions.push(new byte[]{(byte) (minX + i), minY});
+                    validPositions.push(new byte[]{maxX, (byte) (maxY - i)});
+                    validPositions.push(new byte[]{(byte) (maxX - i), maxY});
+                }
+            }
+            Stack<byte[]> auxValidPositions = new Stack<>();
+            while (!validPositions.isEmpty()) {
+                if (isValid(validPositions.peek()[0], validPositions.peek()[1], previousPos)){
+                    auxValidPositions.push(validPositions.peek());
+                }
+                validPositions.pop();
+            }
+            validPositions = auxValidPositions;
+            for (int i = 0; i < stacks.length; i++) {
+                if (stacks[i] == null || stacks[i].isEmpty()) {
+                    stacks[i] = validPositions;
+                    break;
+                }
+            }
+
+            while (!auxValidPositions.isEmpty()){
+                backTracking(auxValidPositions.peek(), (byte) (n-1), HorsePosition, stacks, paths);
+            }
+            if (!stacks[0].isEmpty())
+                stacks[stacks.length - 1 - n].pop();
+        }
+
+    }
+
+    private static  boolean isValid(byte x, byte y, byte[] previousPos){
+        if (x < 8 && y < 8 && x >= 0 && y >= 0) {
+            return !(x == previousPos[0] && y == previousPos[1]);
+        }
+        return false;
+    }
 }
